81b9630131f31499cc55eaa77fd340a987a8c18b
Ali Elouafiq <ali@ali.ma>
2015-11-18 13:22:19 +0000

    changing redirects

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(function(){
 	function createChatBtn(){
-		var title = $('.page-header').text().replace(/ /g,'');
+		//var title = $('.page-header').text().replace(/ /g,'');
         var add_element_button = $("<button />", {
             text: 'Chat with buyer',
             class: "btn btn-block btn-green",
-            click: function() {window.location.href = "http://stackoverflow.com/"+title}
+            click: function() {window.location.href = "http://localhost:3000/"}
         });
         $("<i class='fa fa-comment'></i>").insertBefore()
         $(add_element_button).insertAfter( "#adreply_sidebar_btn" );
